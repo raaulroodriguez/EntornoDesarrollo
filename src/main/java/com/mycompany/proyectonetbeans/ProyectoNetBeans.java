c9dc58cf948011f763d51df6fbebc86ab620b793
raaulroodriguez <raulrodriguezaponte1@gmail.com>
2025-04-24 10:49:52 +0200

    Primer Commit

---
+++ src/main/java/com/mycompany/proyectonetbeans/ProyectoNetBeans.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectonetbeans;
+
+/**
+ *
+ * @author raaulroodriguez
+ */
+public class ProyectoNetBeans {
+
+    public static void main(String[] args) {
+        System.out.println("Primer Commit");
+    }
+}
